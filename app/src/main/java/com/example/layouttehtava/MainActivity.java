b044a530bdc5c1af81a102a5e780c75fcba73e7e
Juha Heinonen <44801965+JuhaHeinonen@users.noreply.github.com>
2019-09-19 23:30:36 +0300

    Button1 works

--- app/src/main/java/com/example/layouttehtava/MainActivity.java
+++ app/src/main/java/com/example/layouttehtava/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.layouttehtava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btn1 = findViewById(R.id.button1);
+        final Button btn2 = findViewById(R.id.button2);
+        final Button btn3 = findViewById(R.id.button3);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (count == 0){
+                    btn2.setEnabled(false);
+                    btn3.setEnabled(false);
+                    count++;
+                }
+                else{
+                    btn2.setEnabled(true);
+                    btn3.setEnabled(true);
+                    count--;
+                }
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
